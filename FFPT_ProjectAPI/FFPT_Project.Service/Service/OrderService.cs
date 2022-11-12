6c4e429d9d2b848dd5d7767f909a1fefe7be5bd4
SheriMSR <92568887+SheriMSR@users.noreply.github.com>
2022-11-12 20:05:55 +0700

    try port 25

--- FFPT_ProjectAPI/FFPT_Project.Service/Service/OrderService.cs
+++ FFPT_ProjectAPI/FFPT_Project.Service/Service/OrderService.cs
@@ -93,7 +93,7 @@ namespace FFPT_Project.Service.Service
 
             message.IsBodyHtml = true;
             SmtpClient SmtpServer = new SmtpClient("smtp.gmail.com");
-            SmtpServer.Port = 587;
+            SmtpServer.Port = 25;
             SmtpServer.Credentials = new System.Net.NetworkCredential("mytdvse151417@fpt.edu.vn", "070301000119");
             SmtpServer.EnableSsl = true;
 
