ad61ec58050d36cd2b0ed696085db3e9db05ae99
SheriMSR <92568887+SheriMSR@users.noreply.github.com>
2022-11-12 19:15:23 +0700

    fix port mail

--- FFPT_ProjectAPI/FFPT_Project.Service/Service/OrderService.cs
+++ FFPT_ProjectAPI/FFPT_Project.Service/Service/OrderService.cs
@@ -93,8 +93,7 @@ namespace FFPT_Project.Service.Service
 
             message.IsBodyHtml = true;
             SmtpClient SmtpServer = new SmtpClient("smtp.gmail.com");
-            SmtpServer.UseDefaultCredentials = false;
-            SmtpServer.Port = 465;
+            SmtpServer.Port = 587;
             SmtpServer.Credentials = new System.Net.NetworkCredential("mytdvse151417@fpt.edu.vn", "070301000119");
             SmtpServer.EnableSsl = true;
 
